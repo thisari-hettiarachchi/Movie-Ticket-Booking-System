5903aeebf40ac2f3263cd2c12cc80d3f86a5c2ed
Thisari Hettiarachchi <msthisari@gmail.com>
2026-01-11 13:24:10 +0530

    update

---
+++ client/src/assets/assets.js
@@ -0,0 +1,16 @@
+import logo from "./logo.png";
+import marvelLogo from "./marvelLogo.svg";
+import googlePlay from "./googlePlay.svg";
+import appStore from "./appStore.svg";
+import screenImage from "./screenImage.svg";
+import profile from "./profile.png";
+
+
+export const assets = {
+  logo,
+  marvelLogo,
+  googlePlay,
+  appStore,
+  screenImage,
+  profile,
+};
