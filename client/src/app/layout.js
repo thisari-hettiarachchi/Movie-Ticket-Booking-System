82413ae9fd8b283332237ed1599a1fdf83ee4797
Thisari Hettiarachchi <msthisari@gmail.com>
2026-01-11 15:55:23 +0530

    update

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import NavBar from "@/components/shared/NavBar";
 import Footer from "@/components/shared/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,22 +16,23 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Seatify: Your Digital Gateway to the Big Screen",
+  title: "Seatify | Your Digital Gateway to the Big Screen",
   description:
     "Seatify is a modern movie ticket booking platform that lets you browse movies, select seats, and reserve tickets seamlessly—all in one place.",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">      
-      <body        
-        className={`${geistSans.variable}
-        ${geistMono.variable} antialiased`}
-      >        
-        <NavBar/>        
-        {children}        
-        <Footer/>      
-      </body>   
-    </html>  
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <NavBar/>
+          {children}
+          <Footer/>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- client/src/components/shared/NavBar.jsx
+++ client/src/components/shared/NavBar.jsx
@@ -1,13 +1,16 @@
 
-"use client"
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
 import { assets } from "@/assets/assets";
 import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
+import { useClerk, UserButton, useUser} from "@clerk/nextjs";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
   return (
     <div className="fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5">
       <Link href="/" className="max-md:flex-1">
@@ -68,12 +71,22 @@ const NavBar = () => {
 
       <div className="flex items-center gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+                {!user ? (
+          <button
+            onClick={() => openSignIn()}
+            className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton />
+        )}
       </div>
 
-      <MenuIcon className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer" onClick={()=>setIsOpen(!isOpen)}/>
+      <MenuIcon
+        className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer"
+        onClick={() => setIsOpen(!isOpen)}
+      />
     </div>
   );
 };
