728c755de57106468d1ed007d39eaa72034bee37
BiyangaKalupahana <biyangadilmini1@gmail.com>
2026-01-11 18:32:42 +0530

    booking section

---
+++ client/src/components/sections/MyBookingsSection/BookingsSection.jsx
@@ -0,0 +1,103 @@
+"use client"
+import { dummyBookingData } from '@/data'
+import React, { useEffect, useState } from 'react'
+import Loading from '@/app/loading'
+import BlurCircle from '@/components/shared/BlurCircle'
+import { format } from 'date-fns'
+import isoTimeFormat from '@/lib/isoTimeFormat'
+import Link from 'next/link'
+import { useAppContext } from '@/context/AppContext'
+
+
+const MyBookingsSection = () => {
+    const currency = process.env.NEXT_PUBLIC_CURRENCY
+    const dateFormat = (date) => format(new Date(date), "dd MMMM yyyy");
+
+    const { axios, getToken, user, image_base_url } = useAppContext()
+
+    const [bookings, setBookings] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    const getMyBookings = async () => {
+        try {
+            const { data } = await axios.get('/api/user/bookings', {
+                headers: { Authorization: `Bearer ${await getToken()}` }
+            })
+
+            if (data.success) {
+                setBookings(data.Bookings)
+            }
+
+        } catch (error) {
+            console.log(error)
+
+        }
+
+        setIsLoading(false)
+    }
+
+    useEffect(() => {
+        if (user) {
+            getMyBookings()
+        }
+
+    }, [user])
+
+    if (isLoading) return <Loading />
+
+    return (
+        <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
+            <BlurCircle top='100px' left='-100px' />
+            <BlurCircle bottom='0' left='600px' />
+
+            <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
+
+            {bookings.map((item, index) => (
+                <div
+                    key={index}
+                    className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20 rounded-lg mt-4 p-2 max-w-3xl'
+                >
+                    <div className='flex flex-col md:flex-row'>
+                        <img
+                            src={image_base_url + item.show.movie.poster_path}
+                            alt=""
+                            className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded'
+                        />
+                        <div className='flex flex-col p-4'>
+                            <p className='text-lg font-semibold'>{item.show.movie.title}</p>
+                            <p className='text-gray-400 text-sm'>
+                                {isoTimeFormat(item.show.movie.runtime)}
+                            </p>
+                            <p className='text-gray-400 text-sm mt-auto'>
+                                {dateFormat(item.show.showDateTime)}
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col md:items-end md:text-right justify-between p-4'>
+                        <div className='flex items-center gap-4'>
+                            <p className='text-2xl font-semibold mb-3'>
+                                {currency}{item.amount}
+                            </p>
+                            {!item.isPaid && (
+                                <Link href={item.paymentLink} className='bg-primary px-4 py-1.5 mb-3 text-sm rounded-full font-medium cursor-pointer'>
+                                    Pay Now
+                                </Link>
+                            )}
+                        </div>
+                        <div className='text-sm'>
+                            <p>
+                                <span className='text-gray-400'>Total Tickets:</span> {item.bookedSeats.length}
+                            </p>
+                            <p>
+                                <span className='text-gray-400'>Seat Number:</span> {item.bookedSeats.join(", ")}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default MyBookingsSection
